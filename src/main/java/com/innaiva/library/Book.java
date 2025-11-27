0d4dbea4a37456534a5387c0cf33f18e9dfb5834
Inna Iva <innaivaqa@gmail.com>
2025-11-27 00:04:56 -0500

    Day 2 - Added Library class with basic operations

--- src/main/java/com/innaiva/library/Book.java
+++ src/main/java/com/innaiva/library/Book.java
@@ -6,7 +6,7 @@ public class Book {
     private final String author;
     private boolean available = true;
 
-    public Book(int id, String title,String author){
+    public Book(int id, String title, String author){
         this.id = id;
         this.title = title;
         this.author = author;
---
+++ src/main/java/com/innaiva/library/Library.java
@@ -0,0 +1,69 @@
+package com.innaiva.library;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Library {
+    private final List<Book> books = new ArrayList<>();
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public Book findBookById(int id) {
+        for (Book b : books) {
+            if (b.getId() == id) {
+                return b;
+            }
+        }
+
+        return null; //if not found
+    }
+
+    public void listBooks() {
+        if (books.isEmpty()) {
+            System.out.println("Library is empty.");
+        }
+        for (Book b : books) {
+            b.printInfo();
+        }
+    }
+
+    public boolean borrowBook(int id) {
+        Book book = findBookById(id);
+
+        if (book == null) {
+            System.out.println("Book not found.");
+            return false;
+        }
+
+        if (!book.isAvailable()) {
+            System.out.println("Book already borrowed.");
+            return false;
+        }
+
+        book.setAvailable(false);
+        System.out.println("You borrowed: " + book.getTitle());
+
+        return true;
+    }
+
+    public boolean returnBook(int id) {
+        Book book = findBookById(id);
+
+        if (book == null) {
+            System.out.println("Book not found.");
+            return false;
+        }
+
+        if (book.isAvailable()) {
+            System.out.println("Book is already returned.");
+            return false;
+        }
+
+        book.setAvailable(true);
+        System.out.println("You returned: " + book.getTitle());
+
+        return true;
+    }
+}
