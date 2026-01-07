e7c53cc067bdcd2f9be6c9a2c790c5b9947c6e66
Inna Iva <innaivaqa@gmail.com>
2026-01-06 20:57:11 -0500

    Day 4 - Implemented borrow/return logic with tests

--- src/main/java/com/innaiva/library/Library.java
+++ src/main/java/com/innaiva/library/Library.java
@@ -1,16 +1,18 @@
 package com.innaiva.library;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 public class Library {
+    private Map<Integer, Integer> borrowedBooks = new HashMap<>();
     private final List<Book> books = new ArrayList<>();
+    private final Set<User> users = new HashSet<>();
 
     public void addBook(Book book) {
         books.add(book);
     }
-
+    public void addUser(User user) {
+        users.add(user);
+    }
     public Book findBookById(int id) {
         for (Book b : books) {
             if (b.getId() == id) {
@@ -49,26 +51,30 @@ public class Library {
         return true;
     }
 
-    public boolean returnBook(int id) {
-        Book book = findBookById(id);
+    public void returnBook(int bookId) {
 
-        if (book == null) {
-            System.out.println("Book not found.");
-            return false;
+        if(!borrowedBooks.containsKey(bookId)) {
+            throw new IllegalStateException("This book was not borrowed");
         }
 
-        if (book.isAvailable()) {
-            System.out.println("Book is already returned.");
-            return false;
-        }
-
+        Book book = findBookById(bookId);
         book.setAvailable(true);
-        System.out.println("You returned: " + book.getTitle());
-
-        return true;
+        borrowedBooks.remove(bookId);
     }
 
     public List<Book> getBooks() {
         return books;
     }
+
+    public void borrowBook (int bookId, int userId){
+        Book book = findBookById(bookId);
+
+        if (!book.isAvailable()) {
+            throw new IllegalStateException("Book is already borrowed");
+        }
+
+        book.setAvailable(false);
+        borrowedBooks.put(bookId, userId);
+    }
+
 }
--- src/main/java/com/innaiva/library/LibraryTest.java
+++ src/main/java/com/innaiva/library/LibraryTest.java
@@ -3,13 +3,12 @@ package com.innaiva.library;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
-import static org.junit.Assert.assertThrows;
-
+import static org.junit.Assert.*;
 
 public class LibraryTest {
 
     @Test
-    void testAddBook() {
+    public void testAddBook() {
         Library library = new Library();
         Book book = new Book(1, "TestJava", "Inna Iva");
 
@@ -18,11 +17,53 @@ public class LibraryTest {
     }
 
     @Test
-    void testFindBookNotFound(){
+    public void testFindBookNotFound(){
         Library library = new Library();
 
         assertThrows(IllegalArgumentException.class, () -> {
             library.findBookById(99);
         });
+    }
+
+    @Test
+    public void testBorrowBook(){
+        Library library = new Library();
+        Book book = new Book (1, "Java", "Smith");
+        User user = new User(1, "Sam");
+
+        library.addBook(book);
+        library.addUser(user);
+
+        library.borrowBook(1, 1);
+        assertFalse(book.isAvailable());
+    }
+    @Test
+    public void testShouldNotAllowBorrowingBorrowBook() {
+        Library library = new Library();
+        Book book = new Book(1, "Java", "Smith");
+        User user = new User(1, "Sam");
+
+        library.addBook(book);
+        library.addUser(user);
+        library.borrowBook(1, 1);
+
+        assertThrows(IllegalStateException.class, () -> {
+            library.borrowBook(1, 1);
+        });
+    }
+
+    @Test
+    public void testReturnBook(){
+        Library library = new Library();
+        Book book = new Book(1, "Java", "Smith");
+        User user = new User(1, "Sam");
+
+        library.addBook(book);
+        library.addUser(user);
+        library.borrowBook(1, 1);
+
+        library.returnBook(1);
+
+        assertTrue(book.isAvailable());
     }
 }
